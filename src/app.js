d7a33328505786c56041fca3afd60ae94166c8a0
Brajesh Singh <brajeshsingh2911@gmail.com>
2025-09-28 01:46:06 +0530

    Exploring routes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,26 @@ const express = require("express");
 const app = express();
 
 
+app.get("/user",(req,res) =>{
+    res.send({firstName: "Brajesh",lastName:"Singh"})
+})
+
+app.post("/user",(req,res) =>{
+    console.log("Data saved")
+    res.send("Data saved to DB.")
+})
+
+app.delete("/user",(req,res) =>{
+    console.log("Data saved")
+    res.send("Data deleted successfully.")
+})
+
+
+// This will match all the HTTP method API calls to /hello
+
+
 app.use("/hello",(req,res) =>{
     res.send("Hello Brajesh");
-})
-
-app.use("/hi",(req,res) =>{
-    res.send("Hello Hi...");
-})
-
-app.use("/",(req,res) =>{
-    res.send("Hello world");
 })
 
 app.listen(7000,() =>{
