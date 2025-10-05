034d0739a5f94b4ca3f06d835b92beb7bb173e2f
Brajesh Singh <brajeshsingh2911@gmail.com>
2025-10-06 02:50:35 +0530

    Save user in DB

--- src/app.js
+++ src/app.js
@@ -1,18 +1,38 @@
 const express = require("express");
 
- const connectDB = require("./config/database")
+const connectDB = require("./config/database")
 const app = express();
+const User = require("./models/user")
 
- connectDB()
- .then(() =>{
-    console.log("DB connected successfullly.")
-    app.listen(7000,() =>{
-    console.log("Server is listining on port 3000..")
-});
-}).catch(err =>{
-    console.log("Database can not be established")
+
+app.post("/signup", async (req, res) => {
+
+    const userObj = {
+        firstName: "Virat",
+        lastNmae: "Kohali",
+        emailId: "kingkohali@gmail.com",
+        password: "abc@1234"
+    }
+
+    const user = new User(userObj)
+    try {
+        await user.save();
+        res.send("User added successfully.")
+    } catch (err) {
+        res.status(400).send("Error saving user")
+    }
+
+
 })
 
 
-
+connectDB()
+    .then(() => {
+        console.log("DB connected successfullly.")
+        app.listen(7000, () => {
+            console.log("Server is listining on port 3000..")
+        });
+    }).catch(err => {
+        console.log("Database can not be established")
+    })
 
