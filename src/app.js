b04451b719ba86f4aa7d4e1c9eb7a3ac8e8a98d2
Brajesh Singh <brajeshsingh2911@gmail.com>
2025-10-05 22:25:24 +0530

    Handeling middleware routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user",(req,res,next) =>{
-     // Route handler
-    //  res.send("Route handler one")
-     next()
-},(req,res) =>{ 
-    // Route handler 2
-    console.log("Handling the second response")
-    res.send("2nd response")
+app.get("/admin/getAllData",(req,res) =>{
+
+    //  Logic of fetching all data
+
+    res.send("All data send")
+});
+
+app.get("/admin/deleteUser",(req,res) =>{
+    // Logic to delete data
+    res.send("Data has been deleted successfully.")
 
 })
 
